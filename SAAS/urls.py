95d500673aeef9cc86ef6bf05eb401b5232f1f55
Dev <devkreat@famcorpp.com>
2023-08-17 03:13:03 +0200

    Added register user

--- SAAS/urls.py
+++ SAAS/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('assignment.urls')),
+    # this path are used for user authentication 
     path('students/',include('django.contrib.auth.urls')),
     path('students/',include('students.urls')),
 ]
--- students/views.py
+++ students/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login ,logout 
 from django.contrib import messages
+# to create a user signup import Usercreation form 
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 # Create your views here.
 def login_user(request):
@@ -24,3 +27,21 @@ def logout_user(request):
 	logout(request)
 	messages.success(request,("You were logged out "))
 	return redirect ('login')
+
+# user sign up 
+
+def register_user(request):
+	if request.method == "POST":
+		form = RegisterUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username=form.cleaned_data['username']
+			password=form.cleaned_data['password1']
+			user =authenticate(username=username, password=password)
+			login(request,user)
+			messages.success(request,("Registration Successful "))
+			return redirect('quiz')
+
+	else:
+		form=RegisterUserForm()
+	return render(request, 'authentication/register_user.html',{'form':form})
